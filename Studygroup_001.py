54bcfa64dc3a25d19c717ec30ad4ef34dda2c302
yangchang <tomtomato@naver.com>
2022-05-10 09:24:46 +0900

    Studygroup_001

--- Studygroup_001.py
+++ Studygroup_001.py
@@ -37,37 +37,45 @@ while True:
 student = {}
 fp = open('student.txt', 'w+')
 
-while True :
-    
-    name = input('이름을 입력하세요. > ')
-    
-    if name == 'q' or name == 'ㅂ' :
-        print('종료합니다.')
-        break
-   
+try :
+    print('파일 실행 중...')
     while True :
-        score = input('국어, 영어, 수학 점수를 입력하세요. > ').split()
-        if len(score) != 3 :
+
+        name = input('이름을 입력하세요. > ')
+
+        if not name.isalpha() :
             print('다시 입력하세요.')
             continue
-        for i in score :
-            if not i.isnumeric() :
-                print('다시 입력하세요.')
-                break
-        else :
+        if name == 'q' or name == 'ㅂ' :
+            print('종료합니다.')
             break
-                
-    output = name + ' ' + ' '.join(score) + '\n'
-    fp.write(output)
 
-    fp.seek(0)
-    for line in fp.readlines() :
-        line_list = line.strip('\n').split()
-        student[line_list[0]] = [int(line_list[i]) for i in range(1, 4)]
+        while True :
+            score = input('국어, 영어, 수학 점수를 입력하세요. > ').split()
+            if len(score) != 3 :
+                print('다시 입력하세요.')
+                continue
+            for i in score :
+                if not i.isnumeric() :
+                    print('다시 입력하세요.')
+                    break
+            else :
+                break
 
-    print('이름\t국어\t영어\t수학\t총점\t평균')
-    for name in student :
-        print('{}\t{}\t{}\t{}\t{}\t{:3.2f}'.format(name, student[name][0], student[name][1], student[name][2],                                                   sum(student[name]), sum(student[name]) / 3))
+        output = name + ' ' + ' '.join(score) + '\n'
+        fp.write(output)
+
+        fp.seek(0)
+        for line in fp.readlines() :
+            line_list = line.strip('\n').split()
+            student[line_list[0]] = [int(line_list[i]) for i in range(1, 4)]
+
+        print('이름\t국어\t영어\t수학\t총점\t평균')
+        for name in student :
+            print('{}\t{}\t{}\t{}\t{}\t{:3.2f}'.format(name, student[name][0], student[name][1], student[name][2],                                                       sum(student[name]), sum(student[name]) / 3))
+            
+except :
+    print('오류가 발생하였습니다.')
             
 fp.close()
 
